8f461a7966f9117c2905d673ad2b64b3a56b7f5f
benedek.takacs-nagy@adevinta.com <benedek.takacs-nagy@adevinta.com>
2023-06-10 09:52:00 +0200

    remove sleep

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ echo "Installing, please wait..."
 sudo chmod +x ./MakeComponent.sh
 cp ./MakeComponent.sh /usr/local/bin/MakeComponent
 cp ./ComponentBase.txt /usr/local/lib
-sleep 3
 echo "Installation successful!"
--- uninstall.sh
+++ uninstall.sh
@@ -2,5 +2,4 @@ echo "Uninstalling, please wait..."
 
 rm -rf /usr/local/bin/MakeComponent
 rm -rf /usr/local/lib/ComponentBase.txt
-sleep 3
 echo "Uninstallation successful!"
